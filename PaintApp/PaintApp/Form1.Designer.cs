4b2aea1e92013ef06f599f9e22f14637eefbee73
Thái Bảo An <93032007+Baoandev@users.noreply.github.com>
2023-05-25 20:53:08 +0700

    Add files via upload

---
+++ PaintApp/PaintApp/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace PaintApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.Pen_width = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pick_color = new System.Windows.Forms.Button();
+			this.Btn_triangle = new System.Windows.Forms.Button();
+			this.Btn_clear = new System.Windows.Forms.Button();
+			this.Btn_fill = new System.Windows.Forms.Button();
+			this.Btn_line = new System.Windows.Forms.Button();
+			this.Btn_rectangle = new System.Windows.Forms.Button();
+			this.Btn_ellipse = new System.Windows.Forms.Button();
+			this.Btn_erase = new System.Windows.Forms.Button();
+			this.Btn_pencil = new System.Windows.Forms.Button();
+			this.Btn_color = new System.Windows.Forms.Button();
+			this.DrawingArea = new System.Windows.Forms.PictureBox();
+			this.Btn_save = new System.Windows.Forms.Button();
+			this.Btn_openfile = new System.Windows.Forms.Button();
+			this.Btn_RemoveImg = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.Pen_width)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.DrawingArea)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1268, 30);
+			this.menuStrip1.TabIndex = 8;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.statusStrip1.Location = new System.Drawing.Point(0, 654);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(1268, 22);
+			this.statusStrip1.TabIndex = 9;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// Pen_width
+			// 
+			this.Pen_width.Location = new System.Drawing.Point(894, 57);
+			this.Pen_width.Name = "Pen_width";
+			this.Pen_width.Size = new System.Drawing.Size(72, 22);
+			this.Pen_width.TabIndex = 10;
+			this.Pen_width.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+			this.Pen_width.ValueChanged += new System.EventHandler(this.Pen_width_ValueChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(900, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(57, 20);
+			this.label1.TabIndex = 13;
+			this.label1.Text = "Width";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// pick_color
+			// 
+			this.pick_color.BackColor = System.Drawing.Color.Black;
+			this.pick_color.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pick_color.Location = new System.Drawing.Point(305, 31);
+			this.pick_color.Name = "pick_color";
+			this.pick_color.Size = new System.Drawing.Size(48, 48);
+			this.pick_color.TabIndex = 1;
+			this.pick_color.UseVisualStyleBackColor = false;
+			this.pick_color.Click += new System.EventHandler(this.pick_color_Click);
+			// 
+			// Btn_triangle
+			// 
+			this.Btn_triangle.Image = global::PaintApp.Properties.Resources.Triangle_32px;
+			this.Btn_triangle.Location = new System.Drawing.Point(607, 38);
+			this.Btn_triangle.Name = "Btn_triangle";
+			this.Btn_triangle.Size = new System.Drawing.Size(48, 41);
+			this.Btn_triangle.TabIndex = 12;
+			this.Btn_triangle.UseVisualStyleBackColor = true;
+			this.Btn_triangle.Click += new System.EventHandler(this.Btn_triangle_Click);
+			// 
+			// Btn_clear
+			// 
+			this.Btn_clear.Image = global::PaintApp.Properties.Resources.icons8_broom_32;
+			this.Btn_clear.Location = new System.Drawing.Point(769, 38);
+			this.Btn_clear.Name = "Btn_clear";
+			this.Btn_clear.Size = new System.Drawing.Size(48, 41);
+			this.Btn_clear.TabIndex = 11;
+			this.Btn_clear.UseVisualStyleBackColor = true;
+			this.Btn_clear.Click += new System.EventHandler(this.Btn_clear_Click);
+			// 
+			// Btn_fill
+			// 
+			this.Btn_fill.Image = global::PaintApp.Properties.Resources.icons8_fill_color_30;
+			this.Btn_fill.Location = new System.Drawing.Point(445, 38);
+			this.Btn_fill.Name = "Btn_fill";
+			this.Btn_fill.Size = new System.Drawing.Size(48, 41);
+			this.Btn_fill.TabIndex = 7;
+			this.Btn_fill.UseVisualStyleBackColor = true;
+			this.Btn_fill.Click += new System.EventHandler(this.Btn_fill_Click);
+			// 
+			// Btn_line
+			// 
+			this.Btn_line.Image = global::PaintApp.Properties.Resources.icons8_line_24;
+			this.Btn_line.Location = new System.Drawing.Point(553, 38);
+			this.Btn_line.Name = "Btn_line";
+			this.Btn_line.Size = new System.Drawing.Size(48, 41);
+			this.Btn_line.TabIndex = 6;
+			this.Btn_line.UseVisualStyleBackColor = true;
+			this.Btn_line.Click += new System.EventHandler(this.Btn_line_Click);
+			// 
+			// Btn_rectangle
+			// 
+			this.Btn_rectangle.Image = global::PaintApp.Properties.Resources.Rectangle_Stroked_32px;
+			this.Btn_rectangle.Location = new System.Drawing.Point(661, 38);
+			this.Btn_rectangle.Name = "Btn_rectangle";
+			this.Btn_rectangle.Size = new System.Drawing.Size(48, 41);
+			this.Btn_rectangle.TabIndex = 5;
+			this.Btn_rectangle.UseVisualStyleBackColor = true;
+			this.Btn_rectangle.Click += new System.EventHandler(this.Btn_rectangle_Click);
+			// 
+			// Btn_ellipse
+			// 
+			this.Btn_ellipse.Image = global::PaintApp.Properties.Resources.Circle_32px;
+			this.Btn_ellipse.Location = new System.Drawing.Point(715, 38);
+			this.Btn_ellipse.Name = "Btn_ellipse";
+			this.Btn_ellipse.Size = new System.Drawing.Size(48, 41);
+			this.Btn_ellipse.TabIndex = 4;
+			this.Btn_ellipse.UseVisualStyleBackColor = true;
+			this.Btn_ellipse.Click += new System.EventHandler(this.Btn_ellipse_Click);
+			// 
+			// Btn_erase
+			// 
+			this.Btn_erase.Image = global::PaintApp.Properties.Resources.Erase_32px;
+			this.Btn_erase.Location = new System.Drawing.Point(823, 38);
+			this.Btn_erase.Name = "Btn_erase";
+			this.Btn_erase.Size = new System.Drawing.Size(48, 41);
+			this.Btn_erase.TabIndex = 3;
+			this.Btn_erase.UseVisualStyleBackColor = true;
+			this.Btn_erase.Click += new System.EventHandler(this.Btn_erase_Click);
+			// 
+			// Btn_pencil
+			// 
+			this.Btn_pencil.Image = global::PaintApp.Properties.Resources.Edit_32px;
+			this.Btn_pencil.Location = new System.Drawing.Point(499, 38);
+			this.Btn_pencil.Name = "Btn_pencil";
+			this.Btn_pencil.Size = new System.Drawing.Size(48, 41);
+			this.Btn_pencil.TabIndex = 2;
+			this.Btn_pencil.UseVisualStyleBackColor = true;
+			this.Btn_pencil.Click += new System.EventHandler(this.Btn_pencil_Click);
+			// 
+			// Btn_color
+			// 
+			this.Btn_color.Image = global::PaintApp.Properties.Resources.unnamed__1_;
+			this.Btn_color.Location = new System.Drawing.Point(237, 31);
+			this.Btn_color.Name = "Btn_color";
+			this.Btn_color.Size = new System.Drawing.Size(48, 48);
+			this.Btn_color.TabIndex = 1;
+			this.Btn_color.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+			this.Btn_color.UseVisualStyleBackColor = true;
+			this.Btn_color.Click += new System.EventHandler(this.Btn_color_Click);
+			// 
+			// DrawingArea
+			// 
+			this.DrawingArea.Location = new System.Drawing.Point(0, 100);
+			this.DrawingArea.Name = "DrawingArea";
+			this.DrawingArea.Size = new System.Drawing.Size(1268, 564);
+			this.DrawingArea.TabIndex = 0;
+			this.DrawingArea.TabStop = false;
+			this.DrawingArea.Click += new System.EventHandler(this.DrawingArea_Click);
+			this.DrawingArea.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawingArea_Paint);
+			this.DrawingArea.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DrawingArea_MouseClick);
+			this.DrawingArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DrawingArea_MouseDown);
+			this.DrawingArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DrawingArea_MouseMove);
+			this.DrawingArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DrawingArea_MouseUp);
+			// 
+			// Btn_save
+			// 
+			this.Btn_save.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Btn_save.Location = new System.Drawing.Point(1000, 12);
+			this.Btn_save.Name = "Btn_save";
+			this.Btn_save.Size = new System.Drawing.Size(88, 33);
+			this.Btn_save.TabIndex = 14;
+			this.Btn_save.Text = "SAVE";
+			this.Btn_save.UseVisualStyleBackColor = true;
+			this.Btn_save.Click += new System.EventHandler(this.Btn_save_Click_1);
+			// 
+			// Btn_openfile
+			// 
+			this.Btn_openfile.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Btn_openfile.Location = new System.Drawing.Point(1000, 51);
+			this.Btn_openfile.Name = "Btn_openfile";
+			this.Btn_openfile.Size = new System.Drawing.Size(88, 33);
+			this.Btn_openfile.TabIndex = 15;
+			this.Btn_openfile.Text = "OPEN";
+			this.Btn_openfile.UseVisualStyleBackColor = true;
+			this.Btn_openfile.Click += new System.EventHandler(this.Btn_openfile_Click);
+			// 
+			// Btn_RemoveImg
+			// 
+			this.Btn_RemoveImg.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Btn_RemoveImg.Location = new System.Drawing.Point(1094, 51);
+			this.Btn_RemoveImg.Name = "Btn_RemoveImg";
+			this.Btn_RemoveImg.Size = new System.Drawing.Size(98, 33);
+			this.Btn_RemoveImg.TabIndex = 16;
+			this.Btn_RemoveImg.Text = "REMOVE";
+			this.Btn_RemoveImg.UseVisualStyleBackColor = true;
+			this.Btn_RemoveImg.Click += new System.EventHandler(this.Btn_RemoveImg_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(1268, 676);
+			this.Controls.Add(this.Btn_RemoveImg);
+			this.Controls.Add(this.Btn_openfile);
+			this.Controls.Add(this.Btn_save);
+			this.Controls.Add(this.pick_color);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.Btn_triangle);
+			this.Controls.Add(this.Btn_clear);
+			this.Controls.Add(this.Pen_width);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.Btn_fill);
+			this.Controls.Add(this.Btn_line);
+			this.Controls.Add(this.Btn_rectangle);
+			this.Controls.Add(this.Btn_ellipse);
+			this.Controls.Add(this.Btn_erase);
+			this.Controls.Add(this.Btn_pencil);
+			this.Controls.Add(this.Btn_color);
+			this.Controls.Add(this.DrawingArea);
+			this.Controls.Add(this.menuStrip1);
+			this.Cursor = System.Windows.Forms.Cursors.Default;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Thái Bảo An - 21110365";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.Pen_width)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.DrawingArea)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox DrawingArea;
+		private System.Windows.Forms.Button Btn_color;
+		private System.Windows.Forms.Button Btn_pencil;
+		private System.Windows.Forms.Button Btn_erase;
+		private System.Windows.Forms.Button Btn_ellipse;
+		private System.Windows.Forms.Button Btn_rectangle;
+		private System.Windows.Forms.Button Btn_line;
+		private System.Windows.Forms.Button Btn_fill;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.NumericUpDown Pen_width;
+		private System.Windows.Forms.Button Btn_clear;
+		private System.Windows.Forms.Button Btn_triangle;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button pick_color;
+		private System.Windows.Forms.Button Btn_save;
+		private System.Windows.Forms.Button Btn_openfile;
+		private System.Windows.Forms.Button Btn_RemoveImg;
+	}
+}
+
---
+++ PaintApp/PaintApp/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintApp   // Thái Bảo An - 21110365 - Hoàn thiện ngày 16/4
+{
+
+	public partial class Form1 : Form
+	{
+		private Point Start, End;
+		private List<Triangle> trianglesList;
+		Bitmap bm;
+		Graphics g;
+		bool isDrawing = false;
+		Point px, py;
+		Pen pen = new Pen(Color.Black, 1);
+		Pen erase = new Pen(Color.White, 10);
+		int x, y, sX, sY, cX, cY;
+		ColorDialog cd = new ColorDialog();
+		Color New_Color;
+		bool isObjectSelected = false;
+
+		public enum TOOL
+		{
+			SELECT,
+			PEN,
+			ERASER,
+			FILLCOLOR,
+			LINE,
+			ELLIPSE,
+			RECTANGLE,
+			TRIANGLE
+		}
+
+		public TOOL curTool = TOOL.SELECT;
+		public Form1()
+		{
+			InitializeComponent();
+			DoubleBuffered = true;
+			g = DrawingArea.CreateGraphics();
+			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+			pen.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+			bm = new Bitmap(DrawingArea.Width, DrawingArea.Height);
+			g = Graphics.FromImage(bm);
+			g.Clear(Color.White);
+			DrawingArea.Image = bm;
+			trianglesList = new List<Triangle>();
+			pen.Width = (float)Pen_width.Value;
+		}
+		private void DrawingArea_MouseMove(object sender, MouseEventArgs e)
+		{
+
+
+			if (e.Button == MouseButtons.Left)
+			{
+				if (isDrawing)
+				{
+					if (curTool == TOOL.PEN)
+					{
+						px = e.Location;
+						g.DrawLine(pen, px, py);
+						py = px;
+					}
+					if (curTool == TOOL.ERASER)
+					{
+						px = e.Location;
+						g.DrawLine(erase, px, py);
+						py = px;
+					}
+				}
+				if (curTool == TOOL.TRIANGLE)
+				{
+					End = e.Location;
+					this.Invalidate();
+				}
+
+			}
+			
+
+			DrawingArea.Refresh();
+
+			x = e.X;
+			y = e.Y;
+			sX = e.X - cX;
+			sY = e.Y - cY;
+
+		}
+
+		private void DrawingArea_MouseUp(object sender, MouseEventArgs e)
+		{
+			isDrawing = false;
+
+			sX = x - cX;
+			sY = y - cY;
+
+			if (curTool == TOOL.ELLIPSE)
+			{
+				g.DrawEllipse(pen, cX, cY, sX, sY);
+			}
+			if (curTool == TOOL.RECTANGLE)
+			{
+				g.DrawRectangle(pen, cX, cY, sX, sY);
+			}
+			if (curTool == TOOL.LINE)
+			{
+				g.DrawLine(pen, cX, cY, x, y);
+			}
+			if (curTool == TOOL.TRIANGLE)
+			{
+
+				End = e.Location;
+				this.Invalidate();
+				Point point1 = Start;
+				Point point2 = new Point((Start.X + End.X) / 2, End.Y);
+				Point point3 = End;
+				g.DrawPolygon(pen, new Point[] { point1, point2, point3 });
+			}
+		}
+
+		private void DrawingArea_Paint(object sender, PaintEventArgs e)
+		{
+			Graphics g = e.Graphics;
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+			if (isDrawing)
+			{
+				if (curTool == TOOL.ELLIPSE)
+				{
+					g.DrawEllipse(pen, cX, cY, sX, sY);
+
+				}
+				if (curTool == TOOL.RECTANGLE)
+				{
+					g.DrawRectangle(pen, cX, cY, sX, sY);
+				}
+				if (curTool == TOOL.LINE)
+				{
+					g.DrawLine(pen, cX, cY, x, y);
+				}
+				if (curTool == TOOL.TRIANGLE)
+				{
+
+					Point point1 = Start;
+					Point point2 = new Point((Start.X + End.X) / 2, End.Y);
+					Point point3 = End;
+					g.DrawPolygon(pen, new Point[] { point1, point2, point3 });
+				}
+			}
+		}
+
+		private void DrawingArea_MouseDown(object sender, MouseEventArgs e)
+		{
+			isDrawing = true;
+			Start = End = e.Location;
+			py = e.Location;
+
+			cX = e.X;
+			cY = e.Y;
+
+		}
+		static Point set_Point(PictureBox pb, Point pt)
+		{
+			float px = 1f * pb.Width / pb.Width;
+			float py = 1f * pb.Height / pb.Height;
+			return new Point((int)(pt.X * px), (int)(pt.Y * py));
+		}
+		private void Validate(Bitmap bm, Stack<Point> sp, int x, int y, Color Old_Color, Color New_Color)
+		{
+			Color cx = bm.GetPixel(x, y);
+			if (cx == Old_Color)
+			{
+				sp.Push(new Point(x, y));
+				bm.SetPixel(x, y, New_Color);
+			}
+		}
+		public void Fill(Bitmap bm, int x, int y, Color New_Clr)
+		{
+			Color Old_Color = bm.GetPixel(x, y);
+			Stack<Point> pixel = new Stack<Point>();
+			pixel.Push(new Point(x, y));
+			bm.SetPixel(x, y, New_Clr);
+			if (Old_Color == New_Clr) { return; }
+
+			while (pixel.Count > 0)
+			{
+				Point pt = (Point)pixel.Pop();
+				if (pt.X > 0 && pt.Y > 0 && pt.X < bm.Width - 1 && pt.Y < bm.Height - 1)
+				{
+					Validate(bm, pixel, pt.X - 1, pt.Y, Old_Color, New_Clr);
+					Validate(bm, pixel, pt.X, pt.Y - 1, Old_Color, New_Clr);
+					Validate(bm, pixel, pt.X + 1, pt.Y, Old_Color, New_Clr);
+					Validate(bm, pixel, pt.X, pt.Y + 1, Old_Color, New_Clr);
+				}
+			}
+		}
+		private void Btn_color_Click(object sender, EventArgs e)
+		{
+			cd.ShowDialog();
+			New_Color = cd.Color;
+			DrawingArea.BackColor = cd.Color;
+			pen.Color = cd.Color;
+			pick_color.BackColor = cd.Color;
+		}
+
+		private void Btn_pencil_Click(object sender, EventArgs e)
+		{
+			curTool = TOOL.PEN;
+
+		}
+
+		private void DrawingArea_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (curTool == TOOL.FILLCOLOR)
+			{
+				Point point = set_Point(DrawingArea, e.Location);
+				Fill(bm, point.X, point.Y, New_Color);
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			DialogResult result;
+			result = MessageBox.Show("Bạn có chắc muốn thoát không?", "Exit Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+			if (result == DialogResult.Yes)
+			{
+				Application.Exit();
+			}
+			else
+			{ 
+				e.Cancel = true; 
+			}
+		}
+
+		private void Btn_triangle_Click(object sender, EventArgs e)
+		{
+			curTool = TOOL.TRIANGLE;
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pick_color_Click(object sender, EventArgs e)
+		{
+
+		}
+		
+
+	
+		
+
+		private void Btn_erase_Click(object sender, EventArgs e)
+		{
+			curTool = TOOL.ERASER;
+
+		}
+
+		private void Btn_save_Click_1(object sender, EventArgs e)  // nhập tên file output 
+		{
+			if (DrawingArea.Image != null)
+			{
+				var sdf = new SaveFileDialog();
+				sdf.Filter = "Image(*.png)|*.png";
+				if (sdf.ShowDialog() == DialogResult.OK)
+				{
+					Bitmap btm = bm.Clone(new Rectangle(0, 0, DrawingArea.Width, DrawingArea.Height), bm.PixelFormat);
+					btm.Save(sdf.FileName, ImageFormat.Png);
+					MessageBox.Show("Đã lưu ảnh");
+				}
+				else if (DrawingArea.Image == null)
+				{
+					MessageBox.Show("Hãy vẽ gì đó");
+				}
+			}
+		}
+
+		private void Btn_openfile_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Image Files (*.bmp; *.jpg; *.jpeg, *.png)|*.bmp;*.jpg;*.jpeg;*.png";
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				// tạo một đối tượng Image từ tệp được chọn
+				Image image = Image.FromFile(openFileDialog.FileName);
+				// tạo một PictureBox và thiết lập thuộc tính Image của nó
+				PictureBox pictureBox = new PictureBox();
+				pictureBox.Image = image;
+				// thiết lập kích thước cho PictureBox dựa trên kích thước của hình ảnh
+				pictureBox.Size = image.Size;
+				// thêm PictureBox vào Controls của Panel
+				DrawingArea.Controls.Add(pictureBox);
+			}
+		}
+
+		private void Btn_RemoveImg_Click(object sender, EventArgs e)
+		{
+			if (DrawingArea.Controls.Count > 0)
+			{
+				PictureBox pictureBox = (PictureBox)DrawingArea.Controls[0];
+				DrawingArea.Controls.Remove(pictureBox);
+			}
+		}
+
+		private void DrawingArea_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Btn_ellipse_Click(object sender, EventArgs e)
+		{
+			curTool = TOOL.ELLIPSE;
+
+		}
+
+		private void Btn_rectangle_Click(object sender, EventArgs e)
+		{
+			curTool = TOOL.RECTANGLE;
+
+		}
+
+		private void Btn_line_Click(object sender, EventArgs e)
+		{
+			curTool = TOOL.LINE;
+
+		}
+
+		private void Btn_fill_Click(object sender, EventArgs e)
+		{
+			curTool = TOOL.FILLCOLOR;
+
+		}
+		private void Btn_clear_Click(object sender, EventArgs e)
+		{
+			g.Clear(Color.White);
+			DrawingArea.Image = bm;
+			curTool = TOOL.SELECT;
+			trianglesList.Clear();
+		}
+		private void Pen_width_ValueChanged(object sender, EventArgs e)
+		{
+			pen.Width = (float)Pen_width.Value;
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
